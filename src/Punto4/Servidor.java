06266e5ed2b885184a478e22ec776184bb044f1e
Luiza Gonzalez <lulogove42@gmail.com>
2026-06-07 23:21:54 -0500

    feat: se realizo el servidor de las funciones trigonometricas

--- src/Punto4/Servidor.java
+++ src/Punto4/Servidor.java
@@ -8,10 +8,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
-    public static void main() throws IOException {
+    public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try{
-          serverSocket = new ServerSocket(20213);
+          serverSocket = new ServerSocket(9797);
         }catch (IOException e){
             System.err.println("Could not listen on port: 20213");
             System.exit(1);
@@ -26,13 +26,46 @@ public class Servidor {
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+        String funcionActual = "cos";
         String inputLine, outputLine;
 
-        while ((inputLine = in.readline()) != null){
-            if (in.startsWith("fun:")) {
-                String funcion = in.substring(4);
-            }
-        }
+        while ((inputLine = in.readLine()) != null){
 
+            if (inputLine.startsWith("fun:")) {
+                String nuevaFuncion  = inputLine.substring(4);
+                if (nuevaFuncion.equals("sin") || nuevaFuncion.equals("cos") || nuevaFuncion.equals("tan")){
+                    funcionActual = nuevaFuncion;
+                    out.println("Función cambiada a: " + funcionActual);
+                }else{
+                    System.out.println("Función no reconocida");
+                }
+
+            }else {
+                double numero  = Double.parseDouble(inputLine);
+                double resultado = 0;
+                switch (funcionActual){
+                    case "sin":
+                        resultado = Math.sin(numero);
+                        break;
+
+                    case "cos":
+                        resultado = Math.cos(numero);
+                        break;
+
+                    case "tan":
+                        resultado = Math.tan(numero);
+                        break;
+                    default:
+                        System.out.println("Funcion no disponible");
+                        break;
+                }
+                out.println(resultado);
+            }
+
+        }
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
     }
 }
